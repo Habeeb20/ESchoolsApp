9d1646f44e8cc0e5dec7c61c920157d5d125e196
Habeeb <ademolawaliyu1@gmail.com>
2024-07-09 17:02:08 +0100

    commit

--- api/database/dbConnect.js
+++ api/database/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb = async() => {
     try {
-        const connect=await mongoose.connect(process.env.MONGO)
+        const connect=await mongoose.connect("mongodb+srv://Habeeb:Ademola1234@habeeb.pal57xa.mongodb.net/Eschools?retryWrites=true&w=majority")
         if (connect) {
             console.log("you have successfully connected to the DB")
         } else {
